cd5150246445e0172fc748af6276c2f08e5c2269
mani shankar <manishankar9in@gmail.com>
2026-07-05 15:35:43 +0530

    chore: remove tracked debug and dummy config files

--- frontend/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Firebase configuration for development
-export const firebaseConfig = {
-  apiKey: "AIzaSyB2I3G0e3a4b8b8c9c7d2f5e6c9f3a4d0",
-  authDomain: "smart-complaint-system.firebaseapp.com",
-  projectId: "smart-complaint-system",
-  storageBucket: "smart-complaint-system.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
-};
